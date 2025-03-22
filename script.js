e6e411f16d2dbe37d20342b63fc534f0e013d86a
lxysgp <68497851+lxysgp@users.noreply.github.com>
2025-03-22 12:40:17 +0800

    v1.2

--- script.js
+++ script.js
@@ -1,95 +1,91 @@
-const elems2 = [ 
-    "H", "He", "Li", "Be", "B", "C", "N", "O", "F", "Ne",
-    "Na", "Mg", "Al", "Si", "P", "S", "Cl", "Ar", "K", "Ca",
-    "Sc", "Ti", "V", "Cr", "Mn", "Fe", "Co", "Ni", "Cu", "Zn",
-    "Ga", "Ge", "As", "Se", "Br", "Kr", "Rb", "Sr", "Y", "Zr",
-    "Nb", "Mo", "Tc", "Ru", "Rh", "Pd", "Ag", "Cd", "In", "Sn",
-    "Sb", "Te", "I", "Xe", "Cs", "Ba", "La", "Ce", "Pr", "Nd",
-    "Pm", "Sm", "Eu", "Gd", "Tb", "Dy", "Ho", "Er", "Tm", "Yb",
-    "Lu", "Hf", "Ta", "W", "Re", "Os", "Ir", "Pt", "Au", "Hg",
-    "Tl", "Pb", "Bi", "Po", "At", "Rn", "Fr", "Ra", "Ac", "Th",
-    "Pa", "U", "Np", "Pu", "Am", "Cm", "Bk", "Cf", "Es", "Fm",
-    "Md", "No", "Lr", "Rf", "Db", "Sg", "Bh", "Hs", "Mt", "Ds",
-    "Rg", "Cn", "Nh", "Fl", "Mc", "Lv", "Ts", "Og","a", "b", "e-"
-    , "g" /* gluon */, "l" /* lepton */, "J" /* Joules */, "X" /*X BOSON */, "Z" /*Z BOSON*/, "D" /* deuterium */, "m" /* mass */
-    , "q" /* quark */, "R" /* roentgen */ ];
-const norelems = ["He", "Li", "Be", "Ne",
-    "Na", "Mg", "Al", "Si", "Cl", "Ar", "Ca",
-    "Sc", "Ti", "Cr", "Mn", "Fe", "Co", "Ni", "Cu", "Zn",
-    "Ga", "Ge", "As", "Se", "Br", "Kr", "Rb", "Sr", "Zr",
-    "Nb", "Mo", "Tc", "Ru", "Rh", "Pd", "Ag", "Cd", "In", "Sn",
-    "Sb", "Te", "Xe", "Cs", "Ba", "La", "Ce", "Pr", "Nd",
-    "Pm", "Sm", "Eu", "Gd", "Tb", "Dy", "Ho", "Er", "Tm", "Yb",
-    "Lu", "Hf", "Ta", "Re", "Os", "Ir", "Pt", "Au", "Hg",
-    "Tl", "Pb", "Bi", "Po", "At", "Rn", "Fr", "Ra", "Ac", "Th",
-    "Pa", "Np", "Pu", "Am", "Cm", "Bk", "Cf", "Es", "Fm",
-    "Md", "No", "Lr", "Rf", "Db", "Sg", "Bh", "Hs", "Mt", "Ds",
-    "Rg", "Cn", "Nh", "Fl", "Mc", "Lv", "Ts", "Og" ];
-const fullElementNames = [ "Helium", "Lithium", "Beryllium", "Neon",
-    "Sodium", "Magnesium", "Aluminium", "Silicon", "Chlorine", "Argon", "Calcium",
-    "Scandium", "Titanium", "Chromium", "Manganese", "Iron", "Cobalt", "Nickel", "Copper", "Zinc",
-    "Gallium", "Germanium", "Arsenic", "Selenium", "Bromine", "Krypton", "Rubidium", "Strontium", "Zirconium",
-    "Niobium", "Molybdenum", "Technetium", "Ruthenium", "Rhodium", "Palladium", "Silver", "Cadmium", "Indium", "Tin",
-    "Antimony", "Tellurium", "Xenon", "Cesium", "Barium", "Lanthanum", "Cerium", "Praseodymium", "Neodymium",
-    "Promethium", "Samarium", "Europium", "Gadolinium", "Terbium", "Dysprosium", "Holmium", "Erbium", "Thulium", "Ytterbium",
-    "Lutetium", "Hafnium", "Tantalum", "Rhenium", "Osmium", "Iridium", "Platinum", "Gold", "Mercury",
-    "Thallium", "Lead", "Bismuth", "Polonium", "Astatine", "Radon", "Francium", "Radium", "Actinium", "Thorium",
-    "Protactinium", "Neptunium", "Plutonium", "Americium", "Curium", "Berkelium", "Californium", "Einsteinium", "Fermium",
-    "Mendelevium", "Nobelium", "Lawrencium", "Rutherfordium", "Dubnium", "Seaborgium", "Bohrium", "Hassium", "Meitnerium", "Darmstadtium",
-    "Roentgenium", "Copernicium", "Nihonium", "Flerovium", "Moscovium", "Livermorium", "Tennessine", "Oganesson"];
-const upperelems = [ "HE", "LI", "BE",  "NE",
-    "NA", "MG", "AL", "SI", "CL", "AR", "CA",
-    "SC", "TI", "CR", "MN", "FE", "CO", "NI", "CU", "ZN",
-    "GA", "GE", "AS", "SE", "BR", "KR", "RB", "SR", "ZR",
-    "NB", "MO", "TC", "RU", "RH", "PD", "AG", "CD", "IN", "SN",
-    "SB", "TE", "XE", "CS", "BA", "LA", "CE", "PR", "ND",
-    "PM", "SM", "EU", "GD", "TB", "DY", "HO", "ER", "TM", "YB",
-    "LU", "HF", "TA", "RE", "OS", "IR", "PT", "AU", "HG",
-    "TL", "PB", "BI", "PO", "AT", "RN", "FR", "RA", "AC", "TH",
-    "PA", "NP", "PU", "AM", "CM", "BK", "CF", "ES", "FM",
-    "MD", "NO", "LR", "RF", "DB", "SG", "BH", "HS", "MT", "DS",
-    "RG", "CN", "NH", "FL", "MC", "LV", "TS", "OG"];
+const norelems = ["He", "Li", "Be", "Ne", "Na", "Mg", "Al", "Si", "Cl", "Ar", "Ca",
+"Sc", "Ti", "Cr", "Mn", "Fe", "Co", "Ni", "Cu", "Zn", "Ga", "Ge", "As", "Se", "Br",
+"Kr", "Rb", "Sr", "Zr", "Nb", "Mo", "Tc", "Ru", "Rh", "Pd", "Ag", "Cd", "In", "Sn",
+"Sb", "Te", "Xe", "Cs", "Ba", "La", "Ce", "Pr", "Nd", "Pm", "Sm", "Eu", "Gd", "Tb",
+"Dy", "Ho", "Er", "Tm", "Yb", "Lu", "Hf", "Ta", "Re", "Os", "Ir", "Pt", "Au", "Hg",
+"Tl", "Pb", "Bi", "Po", "At", "Rn", "Fr", "Ra", "Ac", "Th", "Pa", "Np", "Pu", "Am",
+"Cm", "Bk", "Cf", "Es", "Fm", "Md", "No", "Lr", "Rf", "Db", "Sg", "Bh", "Hs", "Mt",
+"Ds", "Rg", "Cn", "Nh", "Fl", "Mc", "Lv", "Ts", "Og"];
+
+const fullElementNames = [/* Same order */ "Helium", "Lithium", "Beryllium", "Neon", "Sodium", "Magnesium", "Aluminium",
+"Silicon", "Chlorine", "Argon", "Calcium", "Scandium", "Titanium", "Chromium", "Manganese", "Iron", "Cobalt", "Nickel",
+"Copper", "Zinc", "Gallium", "Germanium", "Arsenic", "Selenium", "Bromine", "Krypton", "Rubidium", "Strontium",
+"Zirconium", "Niobium", "Molybdenum", "Technetium", "Ruthenium", "Rhodium", "Palladium", "Silver", "Cadmium",
+"Indium", "Tin", "Antimony", "Tellurium", "Xenon", "Cesium", "Barium", "Lanthanum", "Cerium", "Praseodymium",
+"Neodymium", "Promethium", "Samarium", "Europium", "Gadolinium", "Terbium", "Dysprosium", "Holmium", "Erbium",
+"Thulium", "Ytterbium", "Lutetium", "Hafnium", "Tantalum", "Rhenium", "Osmium", "Iridium", "Platinum", "Gold",
+"Mercury", "Thallium", "Lead", "Bismuth", "Polonium", "Astatine", "Radon", "Francium", "Radium", "Actinium",
+"Thorium", "Protactinium", "Neptunium", "Plutonium", "Americium", "Curium", "Berkelium", "Californium", "Einsteinium",
+"Fermium", "Mendelevium", "Nobelium", "Lawrencium", "Rutherfordium", "Dubnium", "Seaborgium", "Bohrium",
+"Hassium", "Meitnerium", "Darmstadtium", "Roentgenium", "Copernicium", "Nihonium", "Flerovium", "Moscovium",
+"Livermorium", "Tennessine", "Oganesson"];
+
+const upperelems = norelems.map(e => e.toUpperCase());
+
 const upperchars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
 const norchars = [ 'a', 'b', 'C', 'D', 'e', 'F', 'g', 'H', 'I', 'J', 'K',
                    'l', 'm', 'N', 'O', 'P', 'q', 'R', 'S','t','U', 'V',
                    'W', 'X', 'Y', 'Z' ];
-const fullcharnames = ["Alpha Particle", "Beta particle", "Carbon", "Deuterium", "electron", "Fluorine", "Gluon", "Hydrogen"
-, "Iodine", "Joule", "Potassium", "Lepton", "Mass", "Nitrogen", "Oxygen", "Phosphorus", "Quark", "Roentgen", "Sulfer",
+
+const fullcharnames = ["Alpha Particle", "Beta particle", "Carbon", "Deuterium", "Electron", "Fluorine", "Gluon", "Hydrogen",
+"Iodine", "Joule", "Potassium", "Lepton", "Mass", "Nitrogen", "Oxygen", "Phosphorus", "Quark", "Roentgen", "Sulfur",
 "Tau", "Uranium", "Vanadium", "Tungsten", "X boson", "Yttrium", "Z boson"];
+
+function getColorClass(name) {
+  if (["Helium", "Neon", "Argon", "Krypton", "Xenon", "Radon"].includes(name)) return 'noble-gas';
+  if (["Electron", "Gluon", "Lepton", "Quark", "Alpha Particle", "Beta particle", "Tau", "X boson", "Z boson"].includes(name)) return 'particle';
+  if (["Hydrogen", "Oxygen", "Carbon", "Nitrogen", "Sulfur", "Phosphorus"].includes(name)) return 'nonmetal';
+  return 'metal';
+}
 
 function convert() {
   const input = document.getElementById("input").value.toUpperCase();
-  let elems = "";
-  let exp = "(";
+  const output = document.getElementById("output");
+  output.innerHTML = "";
   let i = 0;
 
   while (i < input.length) {
-    let found = false;
-    const pair = input.substr(i, 2);
+    const two = input.substr(i, 2);
+    let matched = false;
+
     for (let j = 0; j < upperelems.length; j++) {
-      if (pair === upperelems[j]) {
-        elems += norelems[j];
-        exp += fullElementNames[j] + (i + 2 < input.length ? ", " : "");
+      if (two === upperelems[j]) {
+        const span = document.createElement("span");
+        span.className = `element ${getColorClass(fullElementNames[j])}`;
+        span.innerText = norelems[j];
+        span.setAttribute('data-tooltip', fullElementNames[j]);
+        output.appendChild(span);
         i += 2;
-        found = true;
+        matched = true;
         break;
       }
     }
 
-    if (found) continue;
+    if (matched) continue;
 
-    const single = input[i];
+    const one = input[i];
     for (let j = 0; j < upperchars.length; j++) {
-      if (single === upperchars[j]) {
-        elems += norchars[j];
-        exp += fullcharnames[j] + (i + 1 < input.length ? ", " : "");
-        if (norchars[j] === 'e') elems += '-';
-        i += 1;
+      if (one === upperchars[j]) {
+        const span = document.createElement("span");
+        const label = fullcharnames[j];
+        span.className = `element ${getColorClass(label)}`;
+        span.innerText = norchars[j];
+        span.setAttribute('data-tooltip', label);
+        output.appendChild(span);
+        if (norchars[j] === 'e') span.innerText += '-';
         break;
       }
     }
-  }
 
-  exp += ")";
-  document.getElementById("output").innerText = `${elems} ${exp}`;
+    i++;
+  }
 }
+
+document.getElementById("input").addEventListener("input", convert);
+
+document.getElementById("copyBtn").addEventListener("click", () => {
+  const text = document.getElementById("output").innerText;
+  navigator.clipboard.writeText(text).then(() => {
+    alert("Copied to clipboard!");
+  });
+});
